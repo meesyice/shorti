27625d4acdea871dda39b2b967d7d99b3d2a7973
Mohammed Al-Laktah <meesy.ice@icloud.com>
2021-10-08 11:23:45 +0200

    made footer stick to the bottom

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 function SERVER_ENDPOINT() {
-    return "https://www.shorti.xyz/api"
+  return "http://127.0.0.1:5000/api";
+  //return "https://www.shorti.xyz/api"
 }
 
-export default SERVER_ENDPOINT
+export default SERVER_ENDPOINT;
